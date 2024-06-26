9580d2c770f4b31ba3bfbcec4be0efa22373d761
Daya8686 <dhondidaya8686@gmail.com>
2024-06-26 22:35:37 +0530

    Implemented Basic Spring Security in previous commit.
    Implemented Basic Spring Security in previous commit with hard coded
    username and password and getting user information with username from DB
    so for every user the Todos are different.
    
    got username with this code with Authentication interface
    public String getUsernameFromAuthentication() {
    		Authentication
    authentication=SecurityContextHolder.getContext().getAuthentication();
    		return authentication.getName();
    
    Inside com.todoapplication.util package we have a class UserInfo

--- todo/src/main/java/com/todoapplication/configuration/springSecurityConfiguration.java
+++ todo/src/main/java/com/todoapplication/configuration/springSecurityConfiguration.java
@@ -4,8 +4,6 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
